63f043d4575f271f82028ed97fc31c7a4de19f4e
tamikofukuda <tamikofukuda@hotmail.com>
2020-05-17 08:52:04 -0300

    add mars

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,7 @@
 console.log("Hello World!\n");
+app.get('/mars', function(req, res) {
+    res.send('Hello Mars!\n');
+  });
+app.listen(8080,function) {console.log(' Example app listening port 8080')}
 
+// teste Tami
